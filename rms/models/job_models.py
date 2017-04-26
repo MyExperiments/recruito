ecdc005378d257c94f2277cb94cb7a1be5ff32bc
sufinsha <sufinsha@gmail.com>
2017-04-26 19:16:24 +0530

    # Redesigning Database

--- rms/models/job_models.py
+++ rms/models/job_models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from .recruiter_models import Recruiter
 from .base_models import AbstractBaseModel
 
+class
 class Job(AbstractBaseModel):
     """
     Job
@@ -12,6 +13,10 @@ class Job(AbstractBaseModel):
     last_date = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     recruiter = models.ForeignKey(Recruiter, on_delete=models.CASCADE, null=False)
+    number_of_openings = models.IntegerField(default=0)
+    designation = models.CharField(max_length=100, default='')
+    keywords = models.CharField(max_length=150, blank=True)
+    job_location = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.title
--- rms/models/recruiter_models.py
+++ rms/models/recruiter_models.py
@@ -1,21 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 from .base_models import AbstractBaseModel
-
-class RecruiterSector(AbstractBaseModel):
-    """
-    RecruiterSector
-    """
-    name = models.CharField('Sector', max_length=50)
-
-    def __str__(self):
-        return self.name
 
 class Recruiter(AbstractBaseModel):
     """
-    RecruiterSector
+    Recruiter
     """
-    company = models.CharField('Company', max_length=50)
-    sector = models.ForeignKey(RecruiterSector, on_delete=models.CASCADE)
+    company = models.CharField('Company', max_length=100)
+    address = models.CharField('Address', max_length=200)
+    country = models.CharField('Country', max_length=100)
+    city = models.CharField('District', max_length=100)
+    state = models.CharField('State', max_length=100)
+    zipcode = models.CharField('Pincode / Zipcode', max_length=100)
+    phone_number = models.CharField('Phone Number', max_length=30)
 
     def __str__(self):
         return self.company
+
+class RecruiterUser(AbstractBaseModel):
+    """
+    Recruiter
+    """
+    recruiter = models.ForeignKey(Recruiter, on_delete=models.CASCADE, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return self.company + "-" + self.user
+
